d79700696a3066c8a873fc5e31bdcfd0f5461a6c
jerecede <jeremias10458@gmail.com>
2025-04-03 12:17:54 +0200

    continuato il tutorial

---
+++ src/app/components/details/details.component.ts
@@ -0,0 +1,41 @@
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HousingService } from '../../services/housing.service';
+import { HousingLocation } from '../../model/housinglocation';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-details',
+  imports: [ReactiveFormsModule],
+  templateUrl: './details.component.html',
+  styleUrl: './details.component.scss'
+})
+
+export class DetailsComponent {
+  route: ActivatedRoute = inject(ActivatedRoute);
+  casettaService: HousingService = inject(HousingService);
+
+  casetta: HousingLocation | undefined;
+
+  // const firstName = new FormControl('');
+  // const lastName = new FormControl('');
+  // const email = new FormControl('');
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl('')
+  });
+
+  constructor() {
+    const housingLocationId = Number(this.route.snapshot.params['id']); //html non ha bisogno di questo
+    this.casetta = this.casettaService.getHousingLocationById(housingLocationId) //this puo essere usato da html
+  }
+
+  submitApplication() {
+    this.casettaService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? '',
+    );
+  }
+}
